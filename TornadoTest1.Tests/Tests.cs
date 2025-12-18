d670285c74c17fa484c2ce701b2421e23ba437d8
Elite <Elite@HUNTERLUCIUSPC>
2025-12-18 19:08:00 +0700

    Add project files.

---
+++ TornadoTest1.Tests/Tests.cs
@@ -0,0 +1,87 @@
+using NUnit.Framework;
+using System;
+
+namespace TornadoTest1.Tests
+{
+    [TestFixture]
+    public class ElevatorTests
+    {
+        [TestCase(0)]
+        public void Constructor_MinFloorLessThan1_ThrowsException(int minFloor)
+        {
+            Assert.Throws<ArgumentException>(() => new Elevator(minFloor: minFloor));
+        }
+
+        [TestCase(5, 1)]
+        [TestCase(2, 2)]
+        public void Constructor_MinFloorGreaterOrEqualThanMaxFloor_ThrowsException(int minFloor, int maxFloor)
+        {
+            Assert.Throws<ArgumentException>(() => new Elevator(minFloor: minFloor, maxFloor: maxFloor));
+        }
+
+        [TestCase(20, 1, 9)]
+        public void Constructor_StartFloorOutOfBounds_ThrowsException(int startFloor, int minFloor, int maxFloor)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Elevator(startFloor: startFloor, minFloor: minFloor, maxFloor: maxFloor));
+        }
+
+        [TestCase(-1)]
+        [TestCase(10)]
+        public void Call_FloorOutOfBounds_ThrowsException(int callFloor)
+        {
+            var elevator = new Elevator(startFloor: 1, minFloor: 1, maxFloor: 9);
+            Assert.Throws<ArgumentOutOfRangeException>(() => elevator.Call(callFloor));
+        }
+
+        [TestCaseSource(nameof(ElevatorCases))]
+        public void Tick_ElevatorCases_AllCallsHandled(ElevatorCaseData data)
+        {
+            var elevator = new Elevator(startFloor: data.StartFloor, minFloor: data.MinFloor, maxFloor: data.MaxFloor);
+            for (int i = 0; i < data.ExpectedFloors.Length; i++)
+            {
+                elevator.Tick();
+                Assert.That(elevator.CurrentFloor == data.ExpectedFloors[i], $"Tick {i + 1}: Expected floor {data.ExpectedFloors[i]}, but got {elevator.CurrentFloor}");
+                elevator.Call(data.FloorsCalls[i]);
+            }
+            Assert.That(elevator.HasPendingRequests, Is.False, "All calls should be handled by the end of the sequence");
+            Assert.That(elevator.CurrentDirection, Is.EqualTo(Direction.Idle), "Elevator should be idle at the end of the sequence");
+        }
+
+        public static ElevatorCaseData[] ElevatorCases =
+        [
+            new ElevatorCaseData
+            {
+                StartFloor = 1,
+                ExpectedFloors = [1, 2, 3], // Ожидаемые этажи после каждого тика, для визуалиации и подтверждения движения лифта
+                FloorsCalls    = [3, 0, 0], // Вызовы на этажи после каждого тика. Для простоты пока 1 вызов за тик, 0 означает отсутствие вызова
+            },
+            new ElevatorCaseData
+            {
+                StartFloor = 1,
+                ExpectedFloors = [1, 2, 3, 4, 5, 4, 3, 2],
+                FloorsCalls    = [5, 0, 2, 0, 0, 0, 0, 0],
+            },
+            new ElevatorCaseData
+            {
+                StartFloor = 4,
+                ExpectedFloors = [4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+                FloorsCalls    = [9, 0, 1, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            },
+            new ElevatorCaseData
+            {
+                StartFloor = 8,
+                ExpectedFloors = [8, 7, 6, 5, 4, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5],
+                FloorsCalls    = [5, 9, 3, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0],
+            },
+        ];
+
+        public class ElevatorCaseData
+        {
+            public int StartFloor = 1;
+            public int MinFloor = 1;
+            public int MaxFloor = 9;
+            public int[] ExpectedFloors;
+            public int[] FloorsCalls;
+        }
+    }
+}
---
+++ TornadoTest1/Program.cs
@@ -0,0 +1,133 @@
+﻿// Программа реализует контроллер простого лифта.
+
+// Алгоритм решения:
+//   1. Лифт в приоритете продолжает движение в одном направлении до конца движения по вызову
+//   2. Если достигли конца движения по вызову
+//      2.1. Если вызовов больше нет, то останавливаем движение и ожидаем вызовов
+//      2.2. Если есть вызовы в противоположном направлении, то меняем направление движения
+
+// Для валидации решения подразумеваю выполнение тестов в TornadoTest1.Tests проекте
+    // Смотри Tick_ElevatorCases_AllCallsHandled и данные в ElevatorCases
+    // Испольняемый файл этого модуля не собираю и не прикладываю т.к. вся валидация в тестах
+
+namespace TornadoTest1
+{
+    public enum Direction { Idle = 0, Up = 1, Down = -1 }
+
+    public class Elevator
+    {
+        public readonly int MinFloor;
+        public readonly int MaxFloor;
+
+        public int CurrentFloor { get; private set; }
+        public Direction CurrentDirection { get; private set; } = Direction.Idle;
+
+        private readonly HashSet<int> Calls = [];
+
+        public Elevator(int startFloor = 1, int minFloor = 1, int maxFloor = 9)
+        {
+            if (minFloor <= 0)
+            {
+                throw new ArgumentException("minFloor must be greater than 0");
+            }
+            if (minFloor >= maxFloor)
+            {
+                throw new ArgumentException("minFloor must be less than maxFloor");
+            }
+            if (startFloor < minFloor || startFloor > maxFloor)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startFloor));
+            }
+            MinFloor = minFloor;
+            MaxFloor = maxFloor;
+            CurrentFloor = startFloor;
+            CurrentDirection = Direction.Idle;
+        }
+
+        public void Call(int floor)
+        {
+            if (floor == 0)
+            {
+                // примем за 0 что кнопку не нажимают
+                return;
+            }
+            if (floor < MinFloor || floor > MaxFloor)
+            {
+                throw new ArgumentOutOfRangeException(nameof(floor));
+            }
+            Calls.Add(floor);
+        }
+
+        public void Tick()
+        {
+            CurrentDirection = DecideNextDirection(CurrentDirection);
+            if (CurrentDirection == Direction.Idle)
+            {
+                return;
+            }
+
+            int nextFloor = Math.Clamp(MinFloor, CurrentFloor + (int)CurrentDirection, MaxFloor);
+            CurrentFloor = nextFloor;
+            Calls.Remove(CurrentFloor);
+
+            if (!HasPendingRequests)
+            {
+                CurrentDirection = Direction.Idle;
+            }
+        }
+
+        private Direction DecideNextDirection(Direction currentDirection)
+        {
+            if (!HasPendingRequests)
+            {
+                return Direction.Idle;
+            }
+
+            if (CurrentDirection == Direction.Idle || CurrentDirection == Direction.Up)
+            {
+                if (HasCallsAbove)
+                {
+                    return Direction.Up;
+                }
+                else
+                {
+                    return Direction.Down;
+                }
+            }
+
+            if (CurrentDirection == Direction.Idle || CurrentDirection == Direction.Down)
+            {
+                if (HasCallsBelow)
+                {
+                    return Direction.Down;
+                }
+                else
+                {
+                    return Direction.Up;
+                }
+            }
+
+            return Direction.Idle;
+        }
+
+        private bool HasCallsAbove
+        {
+            get { return Calls.Any(f => f > CurrentFloor); }
+        }
+
+        private bool HasCallsBelow
+        {
+            get { return Calls.Any(f => f < CurrentFloor); }
+        }
+
+        public bool HasPendingRequests => Calls.Count > 0;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Для валидации решения подразумеваю выполнение тестов в TornadoTest1.Tests проекте. Смотри Tick_ElevatorCases_AllCallsHandled");
+        }
+    }
+}
